3362c1d789125f082cbc56f96fcf5588be0ba5be
Clement Mwimo <mwclemy@Clements-MacBook-Pro.local>
2021-03-26 15:51:09 -0600

    Added a spinner

--- javascript/coreBookService.js
+++ javascript/coreBookService.js
@@ -11,48 +11,102 @@ class BookInfo {
 }
 
 class CoreBookService {
-    static async createNewBook(book) {
-        const res = await fetch(`${API_URL}/books`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(book)
-        });
-        const data = await res.json();
-        const newBook = new BookInfo(data.id, data.title, data.author, data.release_date, data.image);
+    static error = null
+    static response = null;
 
-        return newBook;
+    static async createNewBook(book) {
+        try {
+            const res = await fetch(`${API_URL}/books`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(book)
+            });
+            const data = await res.json();
+            this.response = new BookInfo(data.id, data.title, data.author, data.release_date, data.image);
+        } catch (error) {
+            this.error = error
+        }
+
+        return {
+            error: this.error,
+            data: this.response
+        };
     }
 
     static async getAllBooks() {
-        const res = await fetch(`${API_URL}/books`);
-        const data = await res.json();
-        const allBooks = [];
-        for (let i = 0; i < data.length; i++) {
-            let newBook = new BookInfo(data[i].id, data[i].title, data[i].author, data[i].release_date, data[i].image);
-            allBooks.push(newBook);
+        try {
+            const res = await fetch(`${API_URL}/books`);
+            const data = await res.json();
+            const allBooks = [];
+            for (let i = 0; i < data.length; i++) {
+                let newBook = new BookInfo(data[i].id, data[i].title, data[i].author, data[i].release_date, data[i].image);
+                allBooks.push(newBook);
+            }
+            this.response = allBooks;
+        } catch (error) {
+            this.error = error
         }
-        return allBooks;
+        return {
+
+            error: this.error,
+            data: this.response
+        };
     }
 
     static async getBookInfo(bookId) {
-        const res = await fetch(`${API_URL}/books/${bookId}`);
-        const bookInfo = await res.json();
-        const currentBook = new BookInfo(bookInfo.id, bookInfo.title, bookInfo.author, bookInfo.release_date, bookInfo.image)
+        try {
+            const res = await fetch(`${API_URL}/books/${bookId}`);
+            const bookInfo = await res.json();
+            this.response = new BookInfo(bookInfo.id, bookInfo.title, bookInfo.author, bookInfo.release_date, bookInfo.image);
 
-        return currentBook;
+        } catch (error) {
+            this.error = error
+        }
+
+        return {
+            error: this.error,
+            data: this.response
+        };
     }
 
     static async editBookInfo(bookId, book) {
-        const res = await fetch(`${API_URL}/books/${bookId}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(book)
-        });
+        try {
+            const res = await fetch(`${API_URL}/books/${bookId}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(book)
+            });
+            this.response = null;
 
-        return book;
+        } catch (error) {
+            this.error = error
+        }
+
+        return {
+            error: this.error,
+            data: this.response
+        };
     }
+
+    static async removeBook(currentBookId) {
+        try {
+            const res = await fetch(`${this.API_URL}/books/${currentBookId}`,
+                {
+                    method: 'delete'
+                });
+            this.response = null;
+        } catch (error) {
+            this.error = error;
+        }
+        return {
+            error: this.error,
+            data: this.response
+        };
+
+    }
+
 }
